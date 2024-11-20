c31db5ed2197e09e94440267a5dbd555c00795c1
lumarseijas <lucia.mseijas@alumnos.upm.es>
2024-11-20 19:34:33 +0100

    modified:   lib_vm.py 	modified:   manage-p2.py

--- lib_vm.py
+++ lib_vm.py
@@ -1,7 +1,10 @@
 import logging
-
+#
+from subprocess import call
+#
 log = logging.getLogger('manage-p2')
 
+#comparacion con la del año pasado: nombre es name, MV es VM
 class VM: 
   def __init__(self, name):
     self.name = name
@@ -9,7 +12,12 @@ class VM:
 
   def create_vm (self, image, interfaces):
     # nota: interfaces es un array de diccionarios de python
-    #       aÃ±adir los campos que se necesiten
+    #       añadir los campos que se necesiten
+    #LU:
+    #campos a crear: que copie base-pc1.qcow2 y plantilla
+    call(["qemu-img","create", "-f", "qcow2", "-b", "./cdps-vm-base-pc1.qcow2",  self.name + ".qcow2"])
+    call(["cp", "plantilla-vm-pc1.xml", self.name + ".xml"])
+    #
     log.debug("create_vm " + self.name + " (image: " + image + ")")
     for i in interfaces:
       log.debug("  if: addr=" + i["addr"] + ", mask=" + i["mask"]) 
--- manage-p2.py
+++ manage-p2.py
@@ -1,95 +1,40 @@
 #!/usr/bin/env python
 
 from lib_vm import VM, NET
-import logging
-import sys
+import logging, sys
 
-# Inicialización del log
 def init_log():
+    # Creacion y configuracion del logger
     logging.basicConfig(level=logging.DEBUG)
-    log = logging.getLogger('manage-p2')
+    log = logging.getLogger('auto_p2')
     ch = logging.StreamHandler(sys.stdout)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', "%Y-%m-%d %H:%M:%S")
     ch.setFormatter(formatter)
     log.addHandler(ch)
     log.propagate = False
-    return log
 
-log = init_log()
+def pause():
+    programPause = input("-- Press <ENTER> to continue...")
 
-# Parámetros de configuración de las VMs
-base_image = "cdps-vm-base-pc1.qcow2"
-vm_names = ["C1", "Host", "S1", "S2", "S3"]
+# Main
+init_log()
+print('CDPS - mensaje info1')
+print('Cristina Rodríguez, Elsa Sastre y Lucía Martínez')
 
-# Función para crear el escenario (create)
-def create():
-    log.info("Creando el escenario...")
-    
-    # Crear redes (bridges) para LAN1 y LAN2
-    lan1 = NET("LAN1")
-    lan2 = NET("LAN2")
-    lan1.create()
-    lan2.create()
-    
-    # Configurar cada VM con su dirección IP
-    for vm_name in vm_names:
-        vm = VM(vm_name)
-        ifs = []
-        if vm_name in ["C1", "Host"]:
-            ifs.append({"addr": "10.1.1.{}".format(2 if vm_name == "C1" else 3), "mask": "255.255.255.0"})
-        else:
-            ifs.append({"addr": "10.1.2.{}".format(10 + int(vm_name[-1])), "mask": "255.255.255.0"})
-        
-        # Crear la VM con la imagen base y la configuración de interfaces
-        vm.create_vm(base_image, ifs)
-        log.info(f"Máquina virtual {vm_name} creada.")
+# Ejemplo de creacion de una maquina virtual
+s1 = VM('s1')
+pause()
 
-# Función para arrancar las VMs (start)
-def start():
-    log.info("Arrancando las máquinas virtuales...")
-    for vm_name in vm_names:
-        vm = VM(vm_name)
-        vm.start_vm()
-        log.info(f"Máquina virtual {vm_name} arrancada.")
+ifs = []
+ifs.append( { "addr": "10.11.12.10", "mask": "255.255.255.0" } )
+ifs.append( { "addr": "10.11.13.10", "mask": "255.255.255.0" } )
+s1.create_vm('cdps-vm.qcow2', ifs )
+pause()
 
-# Función para parar las VMs (stop)
-def stop():
-    log.info("Parando las máquinas virtuales...")
-    for vm_name in vm_names:
-        vm = VM(vm_name)
-        vm.stop_vm()
-        log.info(f"Máquina virtual {vm_name} parada.")
+s1.start_vm()
+pause()
 
-# Función para destruir las VMs (destroy)
-def destroy():
-    log.info("Destruyendo el escenario...")
-    for vm_name in vm_names:
-        vm = VM(vm_name)
-        vm.destroy_vm()
-        log.info(f"Máquina virtual {vm_name} destruida.")
+s1.stop_vm()
+pause()
 
-    # Eliminar las redes
-    NET("LAN1").destroy()
-    NET("LAN2").destroy()
-    log.info("Redes LAN1 y LAN2 eliminadas.")
-
-# Ejecución principal del script
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Uso: manage-p2.py <orden>")
-        print("<orden> debe ser: create, start, stop, destroy")
-        sys.exit(1)
-
-    command = sys.argv[1]
-    
-    if command == "create":
-        create()
-    elif command == "start":
-        start()
-    elif command == "stop":
-        stop()
-    elif command == "destroy":
-        destroy()
-    else:
-        log.error("Orden desconocida. Usa create, start, stop o destroy.")
-        sys.exit(1)
+s1.destroy_vm()
