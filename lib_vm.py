adda77f899b664f5c8e6f35fb5744b0f8141d759
lumarseijas <lucia.mseijas@alumnos.upm.es>
2025-01-03 16:26:05 +0100

    modified:   lib_vm.py 	modified:   manage-p2.py

--- lib_vm.py
+++ lib_vm.py
@@ -1,120 +1,161 @@
-#import logging
-#
-import logging, subprocess, os
-#os es para gestionar archivos temporales ;)
-
+import logging
+import os
 from subprocess import call
-#from lxml import etree
-#
-import logging, subprocess, os, tempfile, shutil
+from lxml import etree
+
 log = logging.getLogger('manage-p2')
 
-class VM: 
+def edit_xml(virtualm):
+  if virtualm  == "c1":
+    bridge_aux = "LAN1"
+  elif virtualm == "lb":
+    bridge_aux = "LAN1" 
+  else:
+    bridge_aux = "LAN2"
+  
+
+  cwd = os.getcwd()
+  path = cwd + "/" + virtualm
+
+  tree = etree.parse(path + ".xml")
+
+  root = tree.getroot()
+
+  name = root.find("name")
+  name.text = virtualm
+
+  sourceFile = root.find("./devices/disk/source")
+  sourceFile.set("file", path + ".qcow2")
+
+  interface = root.find("./devices/interface")
+  interface.set("type", "bridge")
+
+  bridge = root.find("./devices/interface/source")
+  bridge.set("bridge", bridge_aux) 
+
+  virtualport = etree.SubElement(interface, "virtualport")
+  virtualport.set("type", "openvswitch")
+
+  interface = root.find("./devices/interface")
+  etree.SubElement(interface, "virtualport", type="openvswitch")
+
+  if virtualm == "lb":
+    devices = root.find("./devices")
+    interface_lan2 = etree.SubElement(devices, "interface", type="bridge")
+    etree.SubElement(interface_lan2, "source", bridge="LAN2")
+    etree.SubElement(interface_lan2, "model", type="virtio")
+    etree.SubElement(interface_lan2, "virtualport", type="openvswitch")
+
+
+
+  #como lb tiene dos interfaces habria que añadir la segunda? o con lo de temporal funciona?
+
+  fout = open(path + ".xml", "w")
+  fout.write(etree.tounicode(tree, pretty_print = True))
+  fout.close()
+
+  '''
+  if virtualm == "lb":
+    fin = open(path + ".xml",'r')   #fin es el XML correspondiente a lb, en modo solo lectura
+    fout = open("tmp.xml",'w')  #fout es un XML temporal abierto en modo escritura
+    for line in fin:
+      if "</interface>" in line:
+        fout.write("</interface>\n <interface type='bridge'>\n <source bridge='"+"LAN2"+"'/>\n <model type='virtio'/>\n<virtualport type = 'openvswitch'/>\n </interface>\n ")
+      #si el XML de lb contiene un interface (que lo va a contener, ya que previamente se le habrá añadido el bridge LAN1), se le añade al XML temporal otro bridge: LAN2
+      else:
+        fout.write(line)
+    fin.close()
+    fout.close()
+
+    call(["cp","./tmp.xml", path + ".xml"])  #sustituimos es XML por el temporal, que es el que contiene las dos LAN
+    call(["rm", "-f", "./tmp.xml"])
+  '''
+def configure(virtualm):
+
+  cwd = os.getcwd()
+  path = cwd + "/" + virtualm
+
+  fout = open("hostname", 'w')
+  fout.write(virtualm + "\n")
+  fout.close()
+  call(["sudo", "virt-copy-in", "-a", virtualm + ".qcow2", "hostname", "/etc"])
+  call(["rm", "-f", "hostname"])
+
+  call("sudo virt-edit -a {virtualm}.qcow2 /etc/hosts -e 's/127.0.1.1.*/127.0.1.1" + virtualm + "/", shell=True) #
+
+  fout = open("interfaces", 'w')
+  if virtualm == "lb":
+    fout.write("auto lo \niface lo inet loopback\n auto eth0\n iface eth0 inet static\n address 10.1.1.1\n netmask 255.255.255.0\nauto eth1\niface eth1 inet static\n  address 10.1.2.1\n netmask 255.255.255.0\n")
+  else:
+    if virtualm == "c1":
+      fout.write("auto lo \niface lo inet loopback\n auto eth0\n iface eth0 inet static\n address 10.1.1.2\n netmask 255.255.255.0\n gateway 10.1.1.1\n")
+
+    else:
+      fout.write("auto lo \niface lo inet loopback\n auto eth0\n iface eth0 inet static\n address 10.1.2.1"+str(virtualm)[1]+"\n netmask 255.255.255.0\n gateway 10.1.2.1\n") #
+  
+  fout.close()
+  call(["sudo", "virt-copy-in", "-a", virtualm + ".qcow2", "interfaces", "/etc/network"])
+  call(["rm", "-f", "interfaces"])
+  if virtualm == "lb":
+        call("sudo virt-edit -a lb.qcow2 /etc/sysctl.conf -e 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/'", shell=True) #0 en vez de 1
+
+
+class VM:
   def __init__(self, name):
     self.name = name
     log.debug('init VM ' + self.name)
-  # def edit_xml(self):
-  #
 
   def create_vm (self, image, interfaces):
     # nota: interfaces es un array de diccionarios de python
     #       añadir los campos que se necesiten
-    #que copie base y plantilla
-    call(["qemu-img","create", "-f", "qcow2", "-b", "./cdps-vm-base-pc1.qcow2",  self.name + ".qcow2"])
-    call(["cp", "plantilla-vm-pc1.xml", self.name + ".xml"])
-   # edit_xml(self.name) #edit_xml está creado arriba: sin terminar
-    call(["sudo", "virsh", "define", self.name + ".xml"])
-
-    #
     log.debug("create_vm " + self.name + " (image: " + image + ")")
     for i in interfaces:
-      log.debug("  if: addr=" + i["addr"] + ", mask=" + i["mask"]) 
-    self.configure_vm_files(interfaces)
-
-  def configure_vm_files(self, interfaces):
-    log.debug(f"Configuring VM files for {self.name}")
-
-    temp_dir = tempfile.mkdtemp()
-    try:
-      # Crear archivos de configuración
-      hostname_path, interfaces_path = self._create_config_files(temp_dir, interfaces)
-      # Copiar archivos a la VM
-      self._copy_config_to_vm(hostname_path, interfaces_path)
-      # Modificar /etc/hosts y habilitar enrutamiento si es necesario
-      self._configure_vm_hosts_and_routing(is_router=(self.name == "lb"))
-    finally:
-      # Limpiar el directorio temporal
-      shutil.rmtree(temp_dir)
-
-  def _create_config_files(self, temp_dir, interfaces):
-    """Crea los archivos /etc/hostname y /etc/network/interfaces."""
-    log.debug(f"Creating config files for {self.name} in {temp_dir}")
-
-    # Crear /etc/hostname
-    hostname_path = os.path.join(temp_dir, "hostname")
-    with open(hostname_path, "w") as f:
-      f.write(self.name + "\n")
-
-    # Crear /etc/network/interfaces
-    interfaces_path = os.path.join(temp_dir, "interfaces")
-    with open(interfaces_path, "w") as f:
-      f.write("auto lo\niface lo inet loopback\n\n")
-      for idx, iface in enumerate(interfaces):
-        f.write(f"auto eth{idx}\n")
-        f.write(f"iface eth{idx} inet static\n")
-        f.write(f"  address {iface['addr']}\n")
-        f.write(f"  netmask {iface['mask']}\n")
-        if "gateway" in iface:
-          f.write(f"  gateway {iface['gateway']}\n")
-        f.write("\n")
+      log.debug("  if: addr=" + i["addr"] + ", mask=" + i["mask"])
+    call(["qemu-img","create", "-f", "qcow2", "-F", "qcow2", "-b", "./cdps-vm-base-pc1.qcow2",  self.name + ".qcow2"])
+    call(["cp", "plantilla-vm-pc1.xml", self.name + ".xml"])
+    virtualm = self.name
+    edit_xml(virtualm)
+    call(["sudo", "virsh", "define", self.name +".xml"])
+    log.debug("Máquinas "+self.name+" definida con éxito.")
+    #virtualm = self.name
+    configure(virtualm)
     
-    return hostname_path, interfaces_path
-
-  def _copy_config_to_vm(self, hostname_path, interfaces_path):
-    """Copia los archivos de configuración a la imagen de la VM."""
-    log.debug(f"Copying config files to {self.name}")
-    # Copiar /etc/hostname
-    call(["sudo", "virt-copy-in", "-a", f"{self.name}.qcow2", hostname_path, "/etc/"])
-    # Copiar /etc/network/interfaces
-    call(["sudo", "virt-copy-in", "-a", f"{self.name}.qcow2", interfaces_path, "/etc/network/"])
-
-  def _configure_vm_hosts_and_routing(self, is_router=False):
-    """Configura /etc/hosts y habilita el enrutamiento si es router."""
-    log.debug(f"Configuring /etc/hosts and routing for {self.name}")
-    # Modificar /etc/hosts
-    call([
-      "sudo", "virt-edit", "-a", f"{self.name}.qcow2", "/etc/hosts",
-      "-e", f"s/127.0.1.1.*/127.0.1.1 {self.name}/"
-    ])
-    # Habilitar enrutamiento si es balanceador
-    if is_router:
-      call([
-        "sudo", "virt-edit", "-a", f"{self.name}.qcow2", "/etc/sysctl.conf",
-        "-e", "'s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/'"
-      ])
-
   def start_vm (self):
     log.debug("start_vm " + self.name)
     call(["sudo", "virsh", "start", self.name])
+    os.system("xterm -e 'sudo virsh console "+ self.name +"' &")
 
   def show_console_vm (self):
     log.debug("show_console_vm " + self.name)
+    os.system("xterm -e 'sudo virsh console "+ self.name +"' &")
 
   def stop_vm (self):
     log.debug("stop_vm " + self.name)
-    call(["sudo", "virsh", "shutdown", self.name])
+    call(["sudo","virsh", "shutdown", self.name])
 
   def destroy_vm (self):
     log.debug("destroy_vm " + self.name)
-    call(["sudo", "virsh", "destroy", self.nombre])
+    call(["sudo", "virsh", "destroy", self.name])
+    call(["sudo", "virsh", "undefine", self.name])
+    call(["rm", "-f", self.name+".qcow2"])
+    call(["rm", "-f", self.name+".xml"])
 
 class NET:
   def __init__(self, name):
     self.name = name
-    log.debug('init net ' + self.name)
+    log.debug(f"Initializing network: {self.name}")
 
   def create_net(self):
-      log.debug('create_net ' + self.name)
-
+    log.debug('create_net ' + self.name)
+    call(["sudo", "ovs-vsctl", "add-br", self.name])
+    call(["sudo", "ifconfig", self.name, "up"])
+    call(["sudo", "ifconfig", "LAN1", "10.1.1.3/24"])
+    call(["sudo", "ip", "route", "add", "10.1.0.0/16", "via", "10.1.1.1"])
+    #call(["sudo", "ifconfig", "LAN1", "10.1.1.3/24"])
+    # call(["sudo", "ip", "link", "set", "dev", self.name, "up"])  
+   
   def destroy_net(self):
-      log.debug('destroy_net ' + self.name)
+    log.debug('destroy_net ' + self.name)
+    call(["sudo", "ifconfig", self.name, "down"])
+   # call(["sudo", "ip", "link", "set", "dev", self.name, "down"])
+    call(["sudo", "ovs-vsctl", "del-br", self.name])
--- manage-p2.py
+++ manage-p2.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
 
 from lib_vm import VM, NET
-import logging, sys, json
+import logging, sys, json, os
+from subprocess import call
+
+# Carga de configuración desde el archivo JSON
+with open('manage-p2.json', 'r') as file:
+    data_json = json.load(file)
+if data_json["number_of_servers"]>5 or data_json["number_of_servers"]<1:
+    print("Número de servidores inválido, introduzca en su archivo de configuración un número de servidores de 1 a 5.")
+else:
+    number_of_servers = data_json["number_of_servers"]
+
+
+debug = data_json["debug"]
+
+if debug == True:
+	logging.basicConfig(level =logging.DEBUG)
+		
+else:
+	logging.basicConfig(level =logging.INFO)
+
+logger = logging.getLogger('manage-p2')
 
 def init_log():
     # Creacion y configuracion del logger
@@ -14,43 +34,168 @@ def init_log():
     log.propagate = False
 
 def pause():
-    programPause = input("-- Press <ENTER> to continue...")
+    input("-- Press <ENTER> to continue...")
 
-with open('manage-p2.json', 'r') as file:
-    data_json = json.load(file)
-
-if data_json["number_of_servers"]>5 or data_json["number_of_servers"]<1:
-    print("Número de servidores inválido, introduzca en su archivo de configuración un número de servidores de 1 a 5.")
-else:
-    num_serv = data_json["number_of_servers"]
-
-# debug = data_json["debug"]
-
-# if debug == True:
-# 	logging.basicConfig(level =logging.DEBUG)
-		
-# else:
-# 	logging.basicConfig(level =logging.INFO)
-     
 # Main
 init_log()
-print('CDPS - mensaje info1')
-#print('Cristina Rodríguez, Elsa Sastre y Lucía Martínez')
+logger.info('CDPS - mensaje info1')
 
-# Ejemplo de creacion de una maquina virtual
-s1 = VM('s1')
-pause()
+def create(number_of_servers):
+  
+    logger.debug("Iniciando creación del escenario.")
+    image = "./cdps-vm-base-pc1.qcow2"
+    number_of_servers = int(number_of_servers)
 
-ifs = []
-ifs.append( { "addr": "10.11.12.10", "mask": "255.255.255.0" } )
-ifs.append( { "addr": "10.11.13.10", "mask": "255.255.255.0" } )
-s1.create_vm('cdps-vm.qcow2', ifs )
-pause()
+    # Crear las redes LAN1 y LAN2
+    logger.debug("Creando redes LAN1 y LAN2.")
+    lan1 = NET('LAN1')
+    lan1.create_net()
+    lan2 = NET('LAN2')
+    lan2.create_net()
 
-s1.start_vm()
-pause()
+    # Crear Cliente c1
+    logger.debug("Creando cliente c1.")
+    interfaces_c1 = [{"addr": "10.1.1.2", "mask": "255.255.255.0"}]
+    c1 = VM("c1")
+    c1.create_vm(image, interfaces_c1)
 
-s1.stop_vm()
-pause()
+    # Crear Balanceador lb
+    logger.debug("Creando balanceador lb.")
+    lb_interfaces = [
+        {"addr": "10.1.1.1", "mask": "255.255.255.0"},
+        {"addr": "10.1.2.1", "mask": "255.255.255.0"}
+    ]
+    lb = VM("lb")
+    lb.create_vm(image, lb_interfaces)
 
-s1.destroy_vm()
+    # Crear Servidores Web
+    logger.debug(f"Creando {number_of_servers} servidores web.")
+    for i in range(1, number_of_servers+1): 
+        server_name = f"s{i}" 
+        logger.debug(f"Creando servidor {server_name}.")
+        server_interfaces = [{"addr": f"10.1.2.1{i}", "mask": "255.255.255.0"}] 
+        server = VM(server_name)
+        server.create_vm(image, server_interfaces)
+    
+    logger.debug("Escenario creado correctamente.")
+
+def start(server):
+    logger.debug(f"Iniciando arranque para: {server}.")
+    if server == "all":
+        logger.debug("Arrancando cliente c1.")
+        c1 = VM('c1')
+        c1.start_vm()
+
+        logger.debug("Arrancando balanceador lb.")
+        lb = VM('lb')
+        lb.start_vm()
+
+        logger.debug(f"Arrancando {number_of_servers} servidores web.")
+        for n in range(1, number_of_servers+1):
+            server_name = f"s{n}"
+            logger.debug(f"Arrancando servidor {server_name}.")
+            server = VM(server_name)
+            server.start_vm()
+
+    else:
+        logger.debug(f"Arrancando máquina específica: {server}.")
+        vm = VM(server)
+        vm.start_vm()
+
+    logger.debug("Arranque completado.")
+
+def stop(server):
+    logger.debug(f"Deteniendo: {server}.")
+    if server == "all":
+        logger.debug("Deteniendo cliente c1.")
+        c1 = VM('c1')
+        c1.stop_vm()
+
+        logger.debug("Deteniendo balanceador lb.")
+        lb = VM('lb')
+        lb.stop_vm()
+
+        logger.debug(f"Deteniendo {number_of_servers} servidores web.")
+        for n in range(1, number_of_servers+1):
+            server_name = f"s{n}"
+            logger.debug(f"Deteniendo servidor {server_name}.")
+            vm = VM(server_name)
+            vm.stop_vm()
+
+    else:
+        logger.debug(f"Deteniendo máquina específica: {server}.")
+        vm = VM(server)
+        vm.stop_vm()
+
+    logger.debug("Detención completada.")
+
+def destroy():
+    logger.debug("Iniciando destrucción del escenario.")
+    logger.debug("Liberando cliente c1.")
+    c1 = VM('c1')
+    c1.destroy_vm()
+
+    logger.debug("Liberando balanceador lb.")
+    lb = VM('lb')
+    lb.destroy_vm()
+
+    logger.debug(f"Liberando {number_of_servers} servidores web.")
+    for n in range(0, number_of_servers+1):
+        server_name = f"s{n+1}"
+        logger.debug(f"Liberando servidor {server_name}.")
+        server = VM(server_name)
+        server.destroy_vm()
+
+    # Eliminar redes
+    logger.debug("Eliminando redes LAN1 y LAN2.")
+    eth0 = NET('LAN1')
+    eth0.destroy_net()
+    eth1 = NET('LAN2')
+    eth1.destroy_net()
+
+    logger.debug("Escenario eliminado correctamente.")
+#MEJORAS:
+def watch(): 
+	os.system("xterm -title monitor -e watch sudo virsh list --all & ")
+
+
+def help(): # print de los comandos y su uso
+    mensaje = """
+        Lista de comandos:
+        create (<number_of_servers>): Crea la red y el número de servidores especificado.
+        start (<server_name>): Arranca todas las máquinas o una específica.
+        stop (<server_name>): Detiene todas las máquinas o una específica.
+        destroy: Elimina todas las máquinas y redes configuradas.
+        watch: Muestra el estado de todas las máquinas virtuales.
+        help: Muestra esta lista de comandos y su funcionalidad.
+    """
+    print(mensaje)
+    
+
+
+# Manejo de argumentos
+arguments = sys.argv
+if len(arguments) == 2:
+    if arguments[1] == "create":
+        create(number_of_servers)
+    if arguments[1] == "start":
+        start("all")
+    if arguments[1] == "stop":
+        stop("all")
+    if arguments[1] == "destroy":
+        destroy()
+    if arguments[1] == "watch":
+        watch()
+    if arguments[1] == "help":
+        help()
+
+if len(arguments) >= 3:
+    if arguments[1] == "create":
+        for server in arguments[2:]:
+            create(server)
+    if arguments[1] == "start":
+        for server in arguments[2:]:
+            start(server)
+    if arguments[1] == "stop":
+        for server in arguments[2:]:
+            stop(server)
